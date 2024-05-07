16c6eb56840917ed970d287e05d1da243e59af6c
George Ghartey <77591764+kuzmanP@users.noreply.github.com>
2024-05-07 22:17:12 +0000

    Setup of user Page

--- VideoApp/url.py
+++ VideoApp/url.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .import views
 
-from .views import index,usersDashboard
+from .views import index,usersDashboard,All_Videos,UploadVideo
 
 urlpatterns=[
-    path('', views.index, name='index'),
+    path('', views.index, name='landing_index'),
     path('userDashboard', views.usersDashboard, name='user_dashboard'),
+    path('/admin/all_videos', views.All_Videos, name='admin_all_videos'),
+    path('/admin/upload_video', views.UploadVideo, name='admin_upload_video'),
 ]
--- VideoApp/views.py
+++ VideoApp/views.py
@@ -5,5 +5,11 @@ from django.shortcuts import render
 def index(request):
     return render(request,'matrix-admin/index2.html')
 
+def All_Videos(request):
+    return render(request,'matrix-admin/All_Videos_Page.html')
+
+def UploadVideo(request):
+    return render(request,'matrix-admin/Upload_Video.html')
+
 def usersDashboard(request):
     return render(request,'matrix-admin/index.html')
